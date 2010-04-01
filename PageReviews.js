59fb054022a8eaa0bd409d813e64bd23853e76b1
Whiteknight <wknight8111@gmail.com>
2010-04-01 15:01:20 -0400

    add the PageReviews javascript file that adds links and stuff to the UI

---
+++ PageReviews.js
@@ -0,0 +1,43 @@
+
+function UserInGroup(group) {
+    var username = wgUserName;
+    var groups = wgUserGroups;
+    for(var i = 0; i < groups.length; i++) {
+        if(groups[i] == group)
+            return true;
+    }
+    return false;
+}
+
+addOnloadHook(function () {
+    if(
+        (UserInGroup('Teachers') || UserInGroup('Administrators')) &&
+        ((wgCanonicalNamespace != "MediaWiki") && (wgCanonicalNamespace != "Special"))
+    ) {
+        addPortletLink('p-cactions', wgServer + wgScript + "?title=Special:PageOwner/" + wgPageName, 'ownership', 'ca-ownership', null, null);
+    }
+});
+
+function AddPersonalLink(link, text, tag, ibefore) {
+    var li = document.createElement( 'li' );
+    li.id = tag;
+    var a = document.createElement( 'a' );
+    a.appendChild( document.createTextNode( text ) );
+    a.href = link;
+    li.appendChild( a );
+    if (!ibefore) {
+        // append to end (right) of list
+        document.getElementById( 'pt-logout' ).parentNode.appendChild( li );
+    } else {
+        var before = document.getElementById( ibefore );
+        before.appendChild( li, before );
+    }
+}
+
+addOnloadHook(function() {
+    AddPersonalLink(wgServer + wgScript + "?title=Special:MyReviews",
+        'my reviews',
+        'pt-myreviews',
+        'pt-mytalk'
+    );
+});
