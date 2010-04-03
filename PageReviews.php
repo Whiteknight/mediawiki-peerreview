5a1e97baab8819c7638a033f7652b97444a6ec0c
Whiteknight <wknight8111@gmail.com>
2010-04-03 09:08:45 -0400

    big cleanup/refactor of PageReviews.php

--- PageReviews.php
+++ PageReviews.php
@@ -10,96 +10,84 @@ $wgHooks['OutputPageParserOutput'][] = array('PageReviews::addReviewForm');
 
 class PageReviews {
 
-   /**
-    * Adds a form to make a page reviewable if it is in the correct namespace, and is already not reviewable.
-    * Does not use the incoming parameters.
-    * @param $out
-    * @param $parseroutput
-    */
-   function addReviewForm(&$out, &$parseroutput) {
+    function addReviewCSS() {
+        global $wgScriptPath, $wgOut;
+        $wgOut->addLink(array(
+            'rel' => 'stylesheet',
+            'type' => 'text/css',
+            'media' =>
+            'screen,projection',
+            'href' => "$wgScriptPath/extensions/PeerReview/PeerReview.css"
+        ));
+    }
+
+    function handlePostBack() {
         global $wgRequest;
         global $wgUser;
-        global $wgTitle;
+        global $wgOut;
         global $wgArticle;
-        global $wgScriptPath, $wgOut;
 
-        //Make sure this is even a reviewability situation (i.e. within the WITTIE content pages)
-        //$namespace = $wgArticle->getTitle()->getNamespace();
-        if(!empty($wgArticle)
-            && $wgArticle->exists()
-            && !$wgArticle->getTitle()->isTalkPage()) {
-            $wgOut->addLink(array(
-                'rel' => 'stylesheet',
-                'type' => 'text/css',
-                'media' =>
-                'screen,projection',
-                'href' => "$wgScriptPath/extensions/PeerReview/PeerReview.css"
-            ));
+        $response = "";
+        $extraClass = "";
 
+        // Review and Comment must be set
+        // User must be logged in
+        if(!$wgRequest->getVal('reviewable-review')
+            || !$wgRequest->getVal('reviewable-comment')
+            || !is_numeric($wgRequest->getVal('reviewable-review'))
+            || $wgUser->getID() == 0) {
 
-            //Handle the POST if necessary
-            if($wgRequest->getVal('reviewable-hidden') == 'go') {
-                $response = "";
-                $extraClass = "";
-
-                //Review and Comment must be set
-                //User must be logged in
-                if(!$wgRequest->getVal('reviewable-review')
-                    || !$wgRequest->getVal('reviewable-comment')
-                    || !is_numeric($wgRequest->getVal('reviewable-review'))
-                    || $wgUser->getID() == 0) {
-
-                    $extraClass = "warning";
-                    if($wgUser->getID() == 0) {
-                        $response = <<<EOT
+            $extraClass = "warning";
+            if($wgUser->getID() == 0) {
+                $response = <<<EOT
     <h2>You must log in to give a review!</h2>
     <p>Please log in by clicking on the &ldquo;log in / create account&rdquo; link at the
         top-right of the page. Thank you.</p>
 EOT;
-                    } else {
-                        $response = <<<EOT
+            } else {
+                $response = <<<EOT
     <h2>Could not add your review!</h2>
     <p>You may have incorrectly filled out the review form. Please try again.</p>
     <p>Remember, you must choose a score <strong>and</strong> write a comment!</p>
 EOT;
-                    }
+            }
 
-                //Else, POST seems valid
-                } else {
-                    $inReview = $wgRequest->getVal('reviewable-review');
-                    $inComment = $wgRequest->getVal('reviewable-comment');
-                    //Check selected review score against database
-                    $dbr = wfGetDB(DB_SLAVE);
-                    $res = $dbr->select('review_score', 'id', 'id=' . $inReview);
-                    $numRows = $dbr->numRows($res);
-                    $dbr->freeResult($res);
-                    if($numRows != 1) {
-                        //The incoming review score isn't in the database
-                        $extraClass = "warning";
-                        $response = <<<EOT
+        // Else, POST seems valid
+        } else {
+            $inReview = $wgRequest->getVal('reviewable-review');
+            $inComment = $wgRequest->getVal('reviewable-comment');
+            // Check selected review score against database
+            $dbr = wfGetDB(DB_SLAVE);
+            $res = $dbr->select('review_score', 'id', 'id=' . $inReview);
+            $numRows = $dbr->numRows($res);
+            $dbr->freeResult($res);
+            if($numRows != 1) {
+                // The incoming review score isn't in the database
+                $extraClass = "warning";
+                $response = <<<EOT
     <h2>Error!</h2>
     <p>Your selected review score is invalid!</p>
 EOT;
-                    } else {
-                        //Good to go
-                        $pageId = $wgArticle->getID();
-                        $dbw = wfGetDB(DB_MASTER);
-                        $toInsert = array(
-                            'page_id' => $pageId,
-                            'user_id' => $wgUser->getID(),
-                            'review_score_id' => $wgRequest->getVal('reviewable-review'),
-                            'comment' => $wgRequest->getVal('reviewable-comment'
-                        ));
-                        $dbw->insert('review', $toInsert);
-                        $extraClass = "success";
-                        $response = <<<EOT
+            } else {
+                // Good to go
+                $pageId = $wgArticle->getID();
+                $dbw = wfGetDB(DB_MASTER);
+                $toInsert = array(
+                    'page_id' => $pageId,
+                    'user_id' => $wgUser->getID(),
+                    'review_score_id' => $wgRequest->getVal('reviewable-review'),
+                    'comment' => $wgRequest->getVal('reviewable-comment'
+                ));
+                $dbw->insert('review', $toInsert);
+                $extraClass = "success";
+                $response = <<<EOT
    <h2>Success!</h2>
    <p>Your review has been added.</p>
 EOT;
-                    }
-                }
+            }
+        }
 
-                $finalResponse = <<<EOT
+        $finalResponse = <<<EOT
 <div id="reviewable-response" class="wittie-box {$extraClass}">
     <div style="padding-top:1em; float:right;">[<a href="#" id="reviewable-response-close">Close</a>]</div>
     {$response}
@@ -110,27 +98,52 @@ EOT;
     });
 </script>
 EOT;
-                $out->addHTML($finalResponse);
-            }
+        $wgOut->addHTML($finalResponse);
+    }
 
-            //Display the form
-            $url = $wgRequest->getFullRequestURL();
+    /**
+     * Adds a form to make a page reviewable if it is in the correct namespace, and is already not reviewable.
+     * Does not use the incoming parameters.
+     * @param $out
+     * @param $parseroutput
+     */
+    function addReviewForm(&$out, &$parseroutput) {
+        global $wgRequest;
+        global $wgUser;
+        global $wgTitle;
+        global $wgArticle;
 
-            //Get <option> list from review_score table
-            $dbr = wfGetDB(DB_SLAVE);
-            $res = $dbr->select('review_score', array('id', 'display_as'));
-            $optionString = "";
-            while($row = $dbr->fetchObject($res)) {
-                $optionString .= "<option value=\"{$row->id}\">{$row->display_as}</option>";
-            }
-            $dbr->freeResult($res);
+        // Make sure this is even a reviewability situation
+        // $namespace = $wgArticle->getTitle()->getNamespace();
+        if(empty($wgArticle)
+            || !$wgArticle->exists()
+            || $wgArticle->getTitle()->isTalkPage()) {
+            return true;
+        }
+        PageReviews::addReviewCSS();
 
-            //Get number of reviews for this page
-            $res = $dbr->select('review', 'id', 'page_id=' . $wgArticle->getID());
-            $numRows = $dbr->numRows($res);
+        // Handle the POST if necessary
+        if($wgRequest->getVal('reviewable-hidden') == 'go') {
+            PageReviews::handlePostBack();
+        }
 
-            //'heredoc' string for HTML insertion
-            $buttonHtml = <<<EOT
+        // Display the form
+        $url = $wgRequest->getFullRequestURL();
+
+        // Get <option> list from review_score table
+        $dbr = wfGetDB(DB_SLAVE);
+        $res = $dbr->select('review_score', array('id', 'display_as'));
+        $optionString = "";
+        while($row = $dbr->fetchObject($res)) {
+            $optionString .= "<option value=\"{$row->id}\">{$row->display_as}</option>";
+        }
+        $dbr->freeResult($res);
+
+        // Get number of reviews for this page
+        $res = $dbr->select('review', 'id', 'page_id=' . $wgArticle->getID());
+        $numRows = $dbr->numRows($res);
+
+        $buttonHtml = <<<EOT
 <div class="reviewable-form-box wittie-box info">
     <h2>Review Page</h2>
     <p>[<a id="reviewable-toggle" href="#">show</a>]</p>
@@ -167,9 +180,8 @@ $('a#reviewable-toggle').click(function() {
 /*]]>*/
 </script>
 EOT;
-            $out->addHTML($buttonHtml);
-        }
+        $out->addHTML($buttonHtml);
         return true;
-    }//(end function)
-}//(end class)
+    }
+}
 ?>
