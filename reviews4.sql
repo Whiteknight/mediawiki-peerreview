b069b5864ba312628da6280fe5b2ade674cc0070
Whiteknight <wknight8111@gmail.com>
2010-04-03 08:36:29 -0400

    add an sql file to populate the review_score table with 4 options

---
+++ reviews4.sql
@@ -0,0 +1,11 @@
+INSERT INTO review_score (id, display_as, value)
+VALUES (1, 1, 1);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (2, 2, 2);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (3, 3, 3);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (4, 4, 4);
