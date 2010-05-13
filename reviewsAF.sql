92709dbe70f7fd90bc51c05877b5bd17eeb77bdd
Whiteknight <wknight8111@gmail.com>
2010-05-13 14:52:01 -0400

    Add a sql file to add some standard letter grades

---
+++ reviewsAF.sql
@@ -0,0 +1,33 @@
+INSERT INTO review_score (id, display_as, value)
+VALUES (1, 'A', 11);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (2, 'A-', 10);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (3, 'B+', 9);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (4, 'B', 8);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (5, 'B-', 7);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (6, 'C+', 6);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (7, 'C', 5);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (8, 'C-', 4);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (9, 'D+', 3);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (10, 'D', 2);
+
+INSERT INTO review_score (id, display_as, value)
+VALUES (11, 'F', 1);
+
