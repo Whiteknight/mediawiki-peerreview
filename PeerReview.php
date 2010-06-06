e2275837d89aa685ce8115c673c2840f782810bd
Whiteknight <wknight8111@gmail.com>
2010-06-06 10:25:40 -0400

    fix script bug where jquery wasn't being loaded correctly

--- PeerReview.php
+++ PeerReview.php
@@ -64,7 +64,7 @@ function PeerReview_addJSAndCSS() {
     #    $wgOut->includeJQuery();
     #} else {
         # MW 1.15 and older
-        $wgOut->addScriptFile("http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+        $wgOut->addScript("<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'></script>");
     #}
 }
 
